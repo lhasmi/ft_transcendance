b65f5e7f7a92840374eee1793290c28d5e5ae725
Pasha Vznuzdajev <pashavznuzdajev@gmail.com>
2024-04-21 08:06:21 +0200

    added tournament upcomingMatches generator

--- src/views/TournamentView.vue
+++ src/views/TournamentView.vue
@@ -5,20 +5,41 @@ import ButtonComp from '../components/ButtonComp.vue'
 // variables
 const players = ref([])
 const playersAmount = ref(0)
-const tournamentRegistration = ref(false)
-const tournamentGame = ref(false)
+const tournamentRegistrationState = ref(false)
+const tournamentGameState = ref(false)
+const tournamentTableState = ref(false)
 const errorMsg = ref('')
+const upcomingMatches = ref([])
 
 // functions
-const toRegistration = (number) => {
+const toRegistrationState = (number) => {
 	playersAmount.value = number
-	tournamentRegistration.value = true
+	tournamentRegistrationState.value = true
 }
 
-const toGame = () => {
+const toTableState = () => {
 	if (!validateInput()) return
-	tournamentGame.value = true
-	tournamentRegistration.value = false
+	tournamentTableState.value = true
+	tournamentGameState.value = false
+	tournamentRegistrationState.value = false
+
+	generateUpcomingMatches()
+}
+
+const generateUpcomingMatches = () => {
+	const playersCopy = players.value.slice(0)
+	let randomPick
+	while (playersCopy.length) {
+		let pair = []
+		randomPick = Math.floor(Math.random() * playersCopy.length)
+		pair[0] = playersCopy[randomPick]
+		playersCopy.splice(randomPick, 1)
+		randomPick = Math.floor(Math.random() * playersCopy.length)
+		pair[1] = playersCopy[randomPick]
+		playersCopy.splice(randomPick, 1)
+		upcomingMatches.value.push(pair)
+	}
+	console.log(upcomingMatches.value)
 }
 
 const validateInput = () => {
@@ -45,10 +66,10 @@ const validateInput = () => {
 <template>
 	<div class="container d-flex flex-grow-1 justify-content-center align-items-center ">
 		<!-- TOURNAMENT REGISTRATION -->
-		<div v-if="tournamentRegistration" class="myshadow bg-white bg-opacity-10 col-11 col-sm-10 col-md-8 col-lg-6 rounded-4">
+		<div v-if="tournamentRegistrationState" class="myshadow bg-white bg-opacity-10 col-11 col-sm-10 col-md-8 col-lg-6 rounded-4">
 			<p class="m-0 mt-3 mb-2 fs-3 roboto-bold text-center" style="color:#f58562">tournament registration</p>
 			<hr class="splitter col-12 mx-auto m-0 mb-4" />
-			<div v-for="n in playersAmount / 2" class="row">
+			<div v-for="n in playersAmount / 2" :key="n" class="row">
 				<div class="col-sm-6 text-center">
 					<span class="roboto-bold fs-5" style="color: #f58562;">{{ n+(n-1) }}.</span>
 					<input v-model="players[n+(n-1)-1]" class="text-input text-white roboto-regular fs-6 p-0 mb-3 text-center" type="text" :id="n+(n-1)"/>
@@ -59,19 +80,28 @@ const validateInput = () => {
 				</div>
 			</div>
 			<div v-if="errorMsg" class="error-msg my-1 fs-6 roboto-bold text-center" style="color: #da4834;">{{ errorMsg }}</div>
-			<ButtonComp @click="toGame" class="btn-lg mt-3 mb-4 fs-5 d-flex justify-content-center mx-auto col-6 col-lg-4">start</ButtonComp>
+			<ButtonComp @click="toTableState" class="btn-lg mt-3 mb-4 fs-5 d-flex justify-content-center mx-auto col-6 col-lg-4">ready</ButtonComp>
+		</div>
+		<!-- TOURNAMENT TABLE -->
+		<div class="myshadow bg-white bg-opacity-10 col-11 col-sm-8 col-md-6 col-lg-4 rounded-4" v-else-if="tournamentTableState">
+			<p class="m-0 mt-3 mb-2 fs-3 roboto-bold text-center" style="color:#f58562">upcoming matches</p>
+			<hr class="splitter col-12 mx-auto m-0 mb-4" />
+			<div v-for="match in upcomingMatches" :key="match[0]">
+				<p class="fs-4 text-white text-center roboto-bold">{{ match[0] }} <span style="color: #f58562;">vs</span> {{ match[1] }}</p>
+			</div>
+			<ButtonComp class="btn-lg mt-3 mb-4 fs-5 d-flex justify-content-center mx-auto col-7 col-lg-5">start</ButtonComp>
 		</div>
 
 		<!-- TOURNAMENT GAME -->
-		<div v-else-if="tournamentGame">tournament game</div>
+		<div v-else-if="tournamentGameState">tournament game</div>
 
 		<!-- CHOOSE AMOUNT OF PLAYERS -->
 		<div v-else class="myshadow bg-white bg-opacity-10 col-11 col-sm-9 col-md-7 col-lg-5 col-xl-4 rounded-4 d-flex flex-column align-items-center">
 			<p class="fs-3 m-0 mt-3 mb-2 roboto-bold text-center" style="color: #f58562;">choose amount of players</p>
 			<hr class="splitter col-12 mx-auto m-0 mb-4" />
 			<div class="mb-4 d-flex">
-				<ButtonComp @click="toRegistration(4)" class="btn-lg fs-3 me-4">4 players</ButtonComp>
-				<ButtonComp @click="toRegistration(8)" class="btn-lg fs-3 ms-4">8 players</ButtonComp>
+				<ButtonComp @click="toRegistrationState(4)" class="btn-lg fs-3 me-4">4 players</ButtonComp>
+				<ButtonComp @click="toRegistrationState(8)" class="btn-lg fs-3 ms-4">8 players</ButtonComp>
 			</div>
 		</div>
 	</div>

89b033e0de2177de3c0d89cd9049c40d97a76ae5
lhasmi <hasmilaila@gmail.com>
2024-05-03 23:20:38 +0200

    admin.py adjusted to recent changes elsewhere

--- transcendance/myapp/admin.py
+++ transcendance/myapp/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from .models import Player, Game
+from .models import Player, Match, FriendRequest
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
 class PlayerAdmin(admin.ModelAdmin):
-    list_display = ('get_username', 'get_email', 'profile_picture', 'created_at')
-    readonly_fields = ('profile_picture',)
+    list_display = ('get_username', 'get_email', 'display_name', 'profile_picture', 'created_at', 'online_status')
+    readonly_fields = ('profile_picture', 'created_at')
+    search_fields = ('user__username', 'user__email', 'display_name')
+    list_filter = ('online_status',)
 
     def get_username(self, obj):
         return obj.user.username
@@ -16,13 +18,26 @@ class PlayerAdmin(admin.ModelAdmin):
         return obj.user.email
     get_email.admin_order_field = 'user__email'
     get_email.short_description = 'Email'
+class MatchAdmin(admin.ModelAdmin):
+    list_display = ('id', 'display_players', 'winner', 'played_on', 'details')
+    search_fields = ('players__user__username', 'winner__user__username')
+    list_filter = ('played_on',)
+    
+    def display_players(self, obj):
+        return ", ".join([player.user.username for player in obj.players.all()])
+    display_players.short_description = 'Players'
+
+class FriendRequestAdmin(admin.ModelAdmin):
+    list_display = ('from_user', 'to_user', 'timestamp')
+    search_fields = ('from_user__user__username', 'to_user__user__username')
+    list_filter = ('timestamp',)
+
+    def from_user(self, obj):
+        return obj.from_user.user.username
+
+    def to_user(self, obj):
+        return obj.to_user.user.username
 
 admin.site.register(Player, PlayerAdmin)
-
-class GameAdmin(admin.ModelAdmin):
-    list_display = ('id', 'player1', 'player2', 'score1', 'score2', 'game_over')
-    list_filter = ('game_over',)
-    search_fields = ('player1__user__username', 'player2__user__username')
-
-admin.site.register(Game, GameAdmin)
-
+admin.site.register(Match, MatchAdmin)
+admin.site.register(FriendRequest, FriendRequestAdmin)

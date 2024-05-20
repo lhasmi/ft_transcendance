b4b48790be88b44964dadbc19ac2d226c74b35e9
Pasha <pashavznuzdajev@gmail.com>
2024-05-21 01:19:02 +0200

    done 3: FriendsModalComp can add friends and check their profile. +minor utils, App.vue

--- src/components/FriendsModalComp.vue
+++ src/components/FriendsModalComp.vue
@@ -4,6 +4,7 @@ import { store } from '../store/store.js'
 import { getText } from '../language/language.js'
 import ButtonComp from './ButtonComp.vue'
 import FriendProfileComp from './FriendProfileComp.vue'
+import { fetchWithJWT, fetchWithJWTJson } from '@/utils/utils.js'
 
 // test data
 let data = {
@@ -12,43 +13,12 @@ let data = {
     { id: 2, username: 'friend2', status: 'online' },
     { id: 3, username: 'friend3', status: 'offline' },
     { id: 4, username: 'friend4', status: 'online' },
-    { id: 5, username: 'friend5', status: 'offline' }
-  ]
+    { id: 5, username: 'friend5', status: 'offline' },
+  ],
 }
 
-let friendData = {
-	id: 1,
-	username: 'friend1',
-	status: 'online',
-	picture: '',
-	gamesHistory: [
-		{
-      id: 3,
-      player1: 'friend1',
-      player2: 'test2',
-      score1: 6,
-      score2: 7,
-      date: '11.04.2024 14:35'
-    },
-		{
-      id: 2,
-      player1: 'friend1',
-      player2: 'test1',
-      score1: 7,
-      score2: 2,
-      date: '11.04.2024 14:35'
-    },
-		{
-      id: 1,
-      player1: 'friend1',
-      player2: 'test3',
-      score1: 3,
-      score2: 7,
-      date: '11.04.2024 14:35'
-    }
-	],
-}
-
+let friendsData
+let friendData
 
 // variables
 const loader = ref(true)
@@ -59,75 +29,69 @@ const errorMsg = ref('')
 
 // funcions
 const loadFriends = async () => {
-	console.log("load friends")
-	loader.value = true
-	try {
-		console.log('1')
-		const response = await fetch('http://127.0.0.1:8000/list-online-friends/', {
-			method: "GET",
-			headers: {
-				'Authorization': `Token ${localStorage.getItem('token')}`,
-			}
-		})
-		console.log('2')
-		const data = await response.json()
-		console.log(data)
-		if (!response.ok) {
-			errorMsg.value = data.error
-		}
-	} catch {
-		errorMsg.value = 'fetch request failed'
-	}
-	loader.value = false
-	// setTimeout(() => { // test
-	// 	loader.value = false
-	// }, 1000);
+  console.log('load friends')
+  loader.value = true
+  try {
+    const response = await fetchWithJWT('http://127.0.0.1:8000/list-friends/')
+    const data = await response.json()
+    console.log(data)
+    if (!response.ok) {
+      errorMsg.value = data.error
+    } else {
+      friendsData = data
+    }
+  } catch {
+    errorMsg.value = 'fetch request failed'
+  }
+  loader.value = false
 }
 
-const loadFriend = async (id) => {
-	loader.value = true
-	// try {
-	// 	const response = await fetch(`127.0.0.1:8000/friend/${id}`)
-	// 	const data = await response.json()
-	// 	console.log(data)
-	// 	if (!response.ok) {
-	// 		errorMsg.value = data.error
-	// 	}
-	// } catch {
-	// 	errorMsg.value = 'fetch request failed'
-	// }
-	// loader.value = false
+// const loadFriend = async (id) => {
+//   loader.value = true;
+//   // try {
+//   // 	const response = await fetch(`127.0.0.1:8000/friend/${id}`)
+//   // 	const data = await response.json()
+//   // 	console.log(data)
+//   // 	if (!response.ok) {
+//   // 		errorMsg.value = data.error
+//   // 	}
+//   // } catch {
+//   // 	errorMsg.value = 'fetch request failed'
+//   // }
+//   // loader.value = false
 
-	setTimeout(() => { // test
-		loader.value = false
-	}, 1000)
-}
+//   setTimeout(() => {
+//     // test
+//     loader.value = false;
+//   }, 1000);
+// };
 
-const addFriend = async (target) => {
-	// send "post" request to try to add a new friend
-	// on success fetch new friends list to rerender it
-	try {
-		const response = await fetch('http://127.0.0.1:8000/send-friend-request/17/', {
-			method: 'POST',
-			headers: {
-				'Authorization': `Token ${localStorage.getItem('token')}`,
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				username: target
-			})
-		})
-		const data = response.json()
-		console.log(data)
-		if (!response.ok) {
-			errorMsg.value = data.error
-		} else {
-
-		}
-	} catch {
-		console.log('fetch request failed')
-		errorMsg.value = 'fetch request failed'
-	}
+const addFriend = async () => {
+  if (friendToAdd.value === '') {
+    return
+  }
+  try {
+    const response = await fetchWithJWTJson(
+      'http://127.0.0.1:8000/add-friend/',
+      'POST',
+      {
+        username: friendToAdd.value,
+      }
+    )
+    const data = await response.json()
+    if (!response.ok) {
+      errorMsg.value = data.detail ? data.detail : data.error
+      console.log(data)
+    } else {
+      console.log(data)
+      loadFriends()
+      friendToAdd.value = ''
+      errorMsg.value = ''
+    }
+  } catch {
+    console.log('fetch request failed')
+    errorMsg.value = 'fetch request failed'
+  }
 }
 
 const getStatusColor = (status) => {
@@ -136,31 +100,32 @@ const getStatusColor = (status) => {
 }
 
 const toFriendProfile = (friendItem) => {
-	loadFriend(friendItem.id)
-  title.value = friendItem.username
-	friendProfile.value = true
-
+  // loadFriend(friendItem.id);
+  title.value = friendItem.user.username
+  friendData = friendItem
+  friendProfile.value = true
 }
 
 const backFriendsModal = () => {
   title.value = 'friends'
-	friendProfile.value = false
-	// loadFriends()
+  friendToAdd.value = ''
+  errorMsg.value = ''
+  friendProfile.value = false
+  // loadFriends()
 }
 
 onMounted(() => {
-	const friendsModal = document.getElementById('friendsModal')
-	friendsModal.addEventListener('show.bs.modal', e => {
-		loadFriends()
-		addFriend('test')
-	})
-	friendsModal.addEventListener('hidden.bs.modal', e => {
-		loader.value = true
-		backFriendsModal()
-		// abort fetch if its still ongoing
-	})
+  const friendsModal = document.getElementById('friendsModal')
+  friendsModal.addEventListener('show.bs.modal', (e) => {
+    loadFriends()
+    // addFriend('test')
+  })
+  friendsModal.addEventListener('hidden.bs.modal', (e) => {
+    loader.value = true
+    backFriendsModal()
+    // abort fetch if its still ongoing
+  })
 })
-
 </script>
 
 <template>
@@ -175,12 +140,14 @@ onMounted(() => {
       <div class="friends-modal modal-content myshadow border-0 rounded-4">
         <div class="modal-header border-0 d-flex flex-column m-0 p-0">
           <div class="d-flex position-relative w-100">
-						<button
+            <button
               v-if="friendProfile"
-              @click="() => {
-								loadFriends()
-								backFriendsModal()
-							}"
+              @click="
+                () => {
+                  loadFriends()
+                  backFriendsModal()
+                }
+              "
               type="button"
               class="icon-back"
             >
@@ -195,46 +162,89 @@ onMounted(() => {
             >
               {{ friendProfile ? title : getText('friends', store.lang) }}
             </h1>
-            <button type="button" class="icon-close" data-bs-dismiss="modal" aria-label="Close">
-              <span class="material-symbols-outlined" style="font-size: 2rem"> close </span>
+            <button
+              type="button"
+              class="icon-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            >
+              <span class="material-symbols-outlined" style="font-size: 2rem">
+                close
+              </span>
             </button>
           </div>
           <hr class="splitter col-12 mx-auto m-0 mb-2" />
         </div>
 
-				<!-- SPINNER -->
-				<div v-if="loader" class="modal-body p-0 d-flex justify-content-center">
-					<div class="spinner-border text-white my-5" role="status">
-  					<span class="visually-hidden">Loading...</span>
-					</div>
-				</div>
-
-				<!-- FRIEND PROFILE -->
-				<FriendProfileComp v-else-if="friendProfile" :data="friendData"/>
-
-				<!-- FRIENDS -->
-        <div v-else class="modal-body p-0 d-flex flex-column" style="max-height: 326px">
-					<div v-if="data.friends.length === 0" class="col-9 col-md-7 mx-auto text-white text-center roboto-regular my-4">
-						{{ getText('friendsListEmpty', store.lang) }}
-					</div>
-          <button
-						class="friend-item col-9 col-md-7 mx-auto d-flex justify-content-center my-2"
-						v-for="item in data.friends"
-						:key="item.id"
-						@click="toFriendProfile(item)"
-          >
-            <p class="friend-item-name roboto-regular fs-5 mb-0">{{ item.username }}</p>
-            <div class="friend-status ms-3 my-auto" :style="getStatusColor(item.status)"></div>
-				</button>
+        <!-- SPINNER -->
+        <div v-if="loader" class="modal-body p-0 d-flex justify-content-center">
+          <div class="spinner-border text-white my-5" role="status">
+            <span class="visually-hidden">Loading...</span>
+          </div>
         </div>
-				<div v-if="!loader && !friendProfile" class="modal-footer p-0 pb-3 mt-2">
-					<hr class="splitter col-12 mx-auto m-0 mb-2" />	
-					<div class="col-9 col-md-7 mx-auto d-flex flex-column">
-						<label class="roboto-bold fs-5 text-center mb-1" style="color: #f58562" for="addFriend">{{ getText('addFriend', store.lang) }}</label>
-						<input v-model="friendToAdd" class="text-input text-white roboto-regular fs-6 p-0 mb-3 text-center" type="text" id="addFriend" :placeholder="getText('username', store.lang)"/>
-						<ButtonComp class="">{{ getText('add', store.lang) }}</ButtonComp>
-					</div>
-      	</div>
+
+        <!-- FRIEND PROFILE -->
+        <FriendProfileComp v-else-if="friendProfile" :data="friendData" />
+
+        <!-- FRIENDS -->
+        <div
+          v-else
+          class="modal-body p-0 d-flex flex-column"
+          style="max-height: 326px"
+        >
+          <div
+            v-if="!friendsData || friendsData.length === 0"
+            class="col-9 col-md-7 mx-auto text-white text-center roboto-regular my-4"
+          >
+            {{ getText('friendsListEmpty', store.lang) }}
+          </div>
+          <button
+            class="friend-item col-9 col-md-7 d-flex mx-auto my-2"
+            v-for="item in friendsData"
+            :key="item.id"
+            @click="toFriendProfile(item)"
+          >
+            <span class="col-2"></span>
+            <p class="friend-item-name roboto-regular col-8 fs-5 mb-0">
+              {{ item.user.username }}
+            </p>
+            <div
+              class="friend-status my-auto col-2 mx-auto"
+              :style="getStatusColor(item.online_status)"
+            ></div>
+          </button>
+        </div>
+        <div
+          v-if="!loader && !friendProfile"
+          class="modal-footer p-0 pb-3 mt-2"
+        >
+          <hr class="splitter col-12 mx-auto m-0 mb-2" />
+          <div class="col-9 col-md-7 mx-auto d-flex flex-column">
+            <label
+              class="roboto-bold fs-5 text-center mb-1"
+              style="color: #f58562"
+              for="addFriend"
+              >{{ getText('addFriend', store.lang) }}</label
+            >
+            <input
+              v-model="friendToAdd"
+              class="text-input text-white roboto-regular fs-6 p-0 mb-3 text-center"
+              type="text"
+              id="addFriend"
+              :placeholder="getText('username', store.lang)"
+            />
+            <div
+              v-if="errorMsg"
+              class="mb-3 fs-6 roboto-bold text-center"
+              style="color: #da4834"
+            >
+              {{ errorMsg }}
+            </div>
+            <ButtonComp @click="addFriend" class="">{{
+              getText('add', store.lang)
+            }}</ButtonComp>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -251,7 +261,12 @@ onMounted(() => {
 }
 
 .friends-modal {
-  background: linear-gradient(145deg, rgba(60, 26, 153, 0.9) 23%, 55%, rgba(92, 42, 132, 0.9) 85%);
+  background: linear-gradient(
+    145deg,
+    rgba(60, 26, 153, 0.9) 23%,
+    55%,
+    rgba(92, 42, 132, 0.9) 85%
+  );
   backdrop-filter: blur(2px);
 }
 
@@ -296,8 +311,8 @@ onMounted(() => {
 }
 
 .friend-item {
-	background: none;
-	border: none;
+  background: none;
+  border: none;
   border-top: 2px solid white;
   border-bottom: 2px solid white;
   transition: all 0.2s ease;
@@ -316,7 +331,7 @@ onMounted(() => {
 }
 
 .modal-footer {
-	border: none;
+  border: none;
 }
 
 input {

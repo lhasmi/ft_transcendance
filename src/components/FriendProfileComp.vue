98dfb228c2069775b8cb3657ece29e3ed059578f
Pasha <pashavznuzdajev@gmail.com>
2024-05-21 01:17:30 +0200

    done 2: FriendsModalComp can add friends and check their profile. +minor utils, App.vue

--- src/components/FriendProfileComp.vue
+++ src/components/FriendProfileComp.vue
@@ -1,76 +1,109 @@
 <script setup>
-import { store } from '../store/store.js'
-import { getText } from '../language/language.js'
+import { store } from "../store/store.js";
+import { getText } from "../language/language.js";
+import { onMounted } from "vue";
 
 const props = defineProps({
-	data: Object
-})
+  data: Object,
+});
+
+const picture = `http://127.0.0.1:8000${props.data.profile_picture}`;
 
 // variables
 
 // functions
 const getStatusColor = (status) => {
-  if (status === 'online') return 'background: #66bf6a'
-  else return 'background: rgba(255, 255, 255, 0.1)'
-}
+  if (status == true) return "background: #66bf6a";
+  else return "background: rgba(255, 255, 255, 0.1)";
+};
 
 const getCircleColor = (index) => {
-  if (index >= props.data.gamesHistory.length) return 'background: rgba(255, 255, 255, 0.1)'
-  return props.data.gamesHistory[index].score1 > props.data.gamesHistory[index].score2
-    ? 'background: #66bf6a'
-    : 'background: #da4834'
-}
+  if (index >= props.data.gamesHistory.length)
+    return "background: rgba(255, 255, 255, 0.1)";
+  return props.data.gamesHistory[index].score1 >
+    props.data.gamesHistory[index].score2
+    ? "background: #66bf6a"
+    : "background: #da4834";
+};
 
+onMounted(() => {
+  console.log(props.data);
+});
 </script>
 
 <template>
-	<div class="modal-body p-0 d-flex flex-column" style="height: 396px">
-		<div class="d-flex">
-			<img
-				src="../assets/profile_img.png"
-				alt="profile image"
-				class="profile-img my-2 mx-auto rounded-4"
-			/>
-		</div>
-		<p class="col-9 col-md-7 mx-auto roboto-bold mb-2 mb-md-3 text-white fs-6">
-			{{ getText('username', store.lang) }}: <span class="fs-6 ms-1">{{ props.data.username }}</span>
-		</p>
-		<div class="col-9 col-md-7 mx-auto roboto-bold mb-2 mb-md-3 text-white fs-6 d-flex ">
-			{{ getText('status', store.lang) }}: <span class="fs-6 ms-1">{{ getText(props.data.status, store.lang) }}</span>
-			<div class="friend-status ms-2 my-auto" :style="getStatusColor(props.data.status)"></div>
-		</div>
-		<hr class="splitter col-12 mx-auto m-0" />
-		<h2 class="fs-3 my-3 mx-auto roboto-bold" style="color: #f58562">{{ getText('lastGames', store.lang) }}</h2>
-		<div class="last_games_circles d-flex col-9 col-md-7 mx-auto justify-content-around mb-3">
-			<div class="circle" :style="getCircleColor(4)"></div>
-			<div class="circle" :style="getCircleColor(3)"></div>
-			<div class="circle" :style="getCircleColor(2)"></div>
-			<div class="circle" :style="getCircleColor(1)"></div>
-			<div class="circle" :style="getCircleColor(0)"></div>
-		</div>
-		<hr class="splitter col-12 mx-auto m-0 mt-3" />
-		<h2 class="fs-3 my-3 mx-auto roboto-bold" style="color: #f58562">{{ getText('gamesHistory', store.lang) }}</h2>
-		<div class="col-9 col-md-7 mx-auto mb-1" v-for="item in props.data.gamesHistory" :key="item.id">
-			<p class="game-date text-center text-white roboto-regular mb-1">{{ item.date }}</p>
-			<div class="d-flex justify-content-center position-relative">
-				<p class="player-left text-white roboto-regular fs-5">{{ item.player1 }}</p>
-				<p class="text-white roboto-bold fs-5">
-					<span
-						class="roboto-bold"
-						:style="item.score1 > item.score2 ? 'color: #f58562' : ''"
-						>{{ item.score1 }}</span
-					>
-					:
-					<span
-						class="roboto-bold"
-						:style="item.score2 > item.score1 ? 'color: #f58562' : ''"
-						>{{ item.score2 }}</span
-					>
-				</p>
-				<p class="player-right text-white roboto-regular fs-5">{{ item.player2 }}</p>
-			</div>
-		</div>
-	</div>
+  <div class="modal-body p-0 d-flex flex-column" style="height: 396px">
+    <div class="d-flex">
+      <img
+        :src="picture"
+        alt="profile image"
+        class="profile-img my-2 mx-auto rounded-4"
+      />
+    </div>
+    <p class="col-9 col-md-7 mx-auto roboto-bold mb-2 mb-md-3 text-white fs-6">
+      {{ getText("username", store.lang) }}:
+      <span class="fs-6 ms-1">{{ props.data.user.username }}</span>
+    </p>
+    <div
+      class="col-9 col-md-7 mx-auto roboto-bold mb-2 mb-md-3 text-white fs-6 d-flex"
+    >
+      {{ getText("status", store.lang) }}:
+      <span class="fs-6 ms-1">{{
+        getText(props.data.online_status ? "online" : "offline", store.lang)
+      }}</span>
+      <div
+        class="friend-status ms-2 my-auto"
+        :style="getStatusColor(props.data.online_status)"
+      ></div>
+    </div>
+    <hr class="splitter col-12 mx-auto m-0" />
+    <h2 class="fs-3 my-3 mx-auto roboto-bold" style="color: #f58562">
+      {{ getText("lastGames", store.lang) }}
+    </h2>
+    <!-- <div
+      class="last_games_circles d-flex col-9 col-md-7 mx-auto justify-content-around mb-3"
+    >
+      <div class="circle" :style="getCircleColor(4)"></div>
+      <div class="circle" :style="getCircleColor(3)"></div>
+      <div class="circle" :style="getCircleColor(2)"></div>
+      <div class="circle" :style="getCircleColor(1)"></div>
+      <div class="circle" :style="getCircleColor(0)"></div>
+    </div> -->
+    <hr class="splitter col-12 mx-auto m-0 mt-3" />
+    <h2 class="fs-3 my-3 mx-auto roboto-bold" style="color: #f58562">
+      {{ getText("gamesHistory", store.lang) }}
+    </h2>
+    <!-- <div
+      class="col-9 col-md-7 mx-auto mb-1"
+      v-for="item in props.data.gamesHistory"
+      :key="item.id"
+    >
+      <p class="game-date text-center text-white roboto-regular mb-1">
+        {{ item.date }}
+      </p>
+      <div class="d-flex justify-content-center position-relative">
+        <p class="player-left text-white roboto-regular fs-5">
+          {{ item.player1 }}
+        </p>
+        <p class="text-white roboto-bold fs-5">
+          <span
+            class="roboto-bold"
+            :style="item.score1 > item.score2 ? 'color: #f58562' : ''"
+            >{{ item.score1 }}</span
+          >
+          :
+          <span
+            class="roboto-bold"
+            :style="item.score2 > item.score1 ? 'color: #f58562' : ''"
+            >{{ item.score2 }}</span
+          >
+        </p>
+        <p class="player-right text-white roboto-regular fs-5">
+          {{ item.player2 }}
+        </p>
+      </div>
+    </div> -->
+  </div>
 </template>
 
 <style scoped>
@@ -117,5 +150,4 @@ const getCircleColor = (index) => {
   scrollbar-color: rgba(255, 255, 255, 0.1) rgba(255, 255, 255, 0.1);
   scrollbar-width: thin;
 }
-
 </style>

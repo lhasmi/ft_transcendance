49de379cabfedd434f009504124b8cda0c10917c
lhasmi <hasmilaila@gmail.com>
2024-05-25 23:04:39 +0200

    added debugging prints of the socket com

--- transcendance/myapp/middleware.py
+++ transcendance/myapp/middleware.py
@@ -15,25 +15,31 @@ from rest_framework_simplejwt.tokens import AccessToken
 from django.contrib.auth.models import AnonymousUser
 from django.contrib.auth import get_user_model
 import urllib.parse
+import logging
 
 User = get_user_model()
+logger = logging.getLogger(__name__) # for debug, willp rint statements of what happens
 
 class TokenAuthMiddleware(BaseMiddleware):
     async def __call__(self, scope, receive, send):
         query_string = scope.get("query_string", b"").decode()
         query_params = dict(urllib.parse.parse_qsl(query_string))
         token = query_params.get("token")
-
+        logger.debug(f"Query string: {query_string}")
+        logger.debug(f"Token: {token}")
         if token:
             try:
                 access_token = AccessToken(token)
                 user_id = access_token["user_id"]
                 user = await self.get_user(user_id)
                 scope["user"] = user
+                logger.debug(f"User authenticated: {user}")
             except Exception as e:
                 scope["user"] = AnonymousUser()
+                logger.debug(f"Authentication failed: {e}")
         else:
             scope["user"] = AnonymousUser()
+            logger.debug("No token provided, anonymous user")
 
         return await super().__call__(scope, receive, send)
 

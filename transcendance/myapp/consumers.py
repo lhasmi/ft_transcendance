9cf7c7ec133dc47cd270f6250859897e0b21e93e
lhasmi <hasmilaila@gmail.com>
2024-05-25 21:45:01 +0200

    see changes, the online status should now recognise the user

--- transcendance/myapp/consumers.py
+++ transcendance/myapp/consumers.py
@@ -2,27 +2,38 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django.contrib.auth import get_user_model
+from channels.db import database_sync_to_async 
 
 User = get_user_model()
 
 class StatusConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_name = 'online_status'
-        self.room_group_name = f'status_{self.room_name}'#unique name for broadcast
-        await self.channel_layer.group_add(# Joining the group to receive messages
-            self.room_group_name,
-            self.channel_name
-        )
-        await self.accept()
-    async def disconnect(self, close_code):# Leaving the group on disconnect
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
-        )
-    async def receive(self, text_data):
+        self.user = self.scope["user"] # Extract user from scope
+        # If the user is authenticated, set them online and accept the connection
+        if self.user.is_authenticated:
+            await self.set_online_status()
+            await self.accept()
+        else:# Close the connection if the user is not authenticated
+            await self.close()
+        # self.room_name = 'online_status'
+        # self.room_group_name = f'status_{self.room_name}'#unique name for broadcast
+        # await self.channel_layer.group_add(# Joining the group to receive messages
+        #     self.room_group_name,
+        #     self.channel_name
+        # )
+        # await self.accept()
+    async def disconnect(self, close_code):
+        # If the user is authenticated, set them offline when they disconnect
+        if self.user.is_authenticated:
+            await self.set_offline_status()
+        # await self.channel_layer.group_discard(
+        #     self.room_group_name,
+        #     self.channel_name
+        # )
+    async def receive(self, text_data):# Handle incoming messages (if needed)
         data = json.loads(text_data)
-        username = data['username']
-        online_status = data['online_status']
+        username = data.get('username')
+        online_status = data.get('online_status')
         # Broadcasting the status update to all connected clients
         await self.channel_layer.group_send(
             self.room_group_name,
@@ -40,3 +51,13 @@ class StatusConsumer(AsyncWebsocketConsumer):
             'username': username,
             'online_status': online_status
         }))
+
+    @database_sync_to_async
+    def set_online_status(self):
+        # Increment the user's online status count
+        self.user.player.set_online()
+
+    @database_sync_to_async
+    def set_offline_status(self):
+        # Decrement the user's online status count
+        self.user.player.set_offline()
--- transcendance/transcendance/asgi.py
+++ transcendance/transcendance/asgi.py
@@ -6,20 +6,20 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
-
 import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from myapp.routing import websocket_urlpatterns
+from myapp.middleware import TokenAuthMiddleware
 from channels.auth import AuthMiddlewareStack 
 # from channels.security.websocket import AllowedHostsOriginValidator
-from myapp.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'transcendance.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     # "websocket": AllowedHostsOriginValidator( #restricts WebSocket connections to the ALLOWED_HOSTS configured in settings.py
-    "websocket": AuthMiddlewareStack(
+    "websocket": TokenAuthMiddleware(
             URLRouter(
                 websocket_urlpatterns
             )

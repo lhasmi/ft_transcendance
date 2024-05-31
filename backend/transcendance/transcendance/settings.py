a9c50706ec3f8d4cbedecbf6da9bf14d61dcd156
lhasmi <hasmilaila@gmail.com>
2024-05-31 20:11:11 +0200

    redis remmoved

--- backend/transcendance/transcendance/settings.py
+++ backend/transcendance/transcendance/settings.py
@@ -93,13 +93,19 @@ ROOT_URLCONF = 'transcendance.urls'
 
 ASGI_APPLICATION = 'transcendance.asgi.application'
 
+# CHANNEL_LAYERS = {
+#     'default': {
+#         'BACKEND': 'channels_redis.core.RedisChannelLayer',
+#         'CONFIG': {
+#             'hosts': [('127.0.0.1', 6379)],
+#         },
+#     },
+# }
+
 CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            'hosts': [('127.0.0.1', 6379)],
-        },
-    },
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
 }
 
 REST_FRAMEWORK = {

3b0c4e158ac9ad16eeed540e0ca48a0f961002f3
lhasmi <hasmilaila@gmail.com>
2024-06-01 16:55:40 +0200

    minor ... nothing really

--- backend/conf/gunicorn_config.py
+++ backend/conf/gunicorn_config.py
@@ -1,3 +1,4 @@
+print("!!!!!!!      this file is accessed !!!!!!!!!!")#debug
 command = '/Users/lailah/transcendance/ft_transcendance/vtrans/bin/gunicorn'
 pythonpath = '/Users/lailah/transcendance/ft_transcendance/transcendance'
 bind = '0.0.0.0:8000'

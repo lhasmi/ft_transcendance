4e7333554421a401ec0ccaa6cc3042a97efdb6d1
Pasha Vznuzdajev <pashavznuzdajev@gmail.com>
2024-06-02 16:01:45 +0200

    deleted unnecessary files, renamed directory to backend

--- conf/gunicorn_config.py
+++
@@ -1,4 +0,0 @@
-command = '/Users/lailah/transcendance/ft_transcendance/vtrans/bin/gunicorn'
-pythonpath = '/Users/lailah/transcendance/ft_transcendance/transcendance'
-bind = '0.0.0.0:8000'
-workers = 3
